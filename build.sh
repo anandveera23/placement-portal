2da44b9e9b4ef8078225b8bece6818c7384a0414
Anand Naidu <anandnaidu296@gmail.com>
2026-02-20 01:34:21 +0530

    Force clean DB

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
+python manage.py flush --noinput
 python manage.py migrate
 
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin','admin@gmail.com','admin123')" | python manage.py shell
